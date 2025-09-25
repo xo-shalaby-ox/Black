91fd3a0aceefb3f492cc4722f04b60ead9f1ebef
xo-shalaby-ox <mshalaby54@yahoo.com>
2025-09-26 00:05:09 +0300

    edit home

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -5,11 +5,13 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div className="relative flex flex-col items-center justify-center z-40  bg-black text-white gap-4">
-        list menu
-        <Link to="signup">sign up</Link>
-        <Link to="login">login</Link>
-        <h1> Welcome Alaa i love you ^_^</h1>
+      <div className="relative flex flex-col items-center justify-center z-40 bg-transparent text-white gap-4">
+        <Link to="/login">login</Link>
+        <Link to="/signup">sign up</Link>
+        <h1 className="text-3xl font-bold text-red-400">
+          {" "}
+          Welcome Alaa i love you ^_^
+        </h1>
       </div>
       <BackgroundBeams />
     </>
