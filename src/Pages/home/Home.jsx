1c1d212418953af19f1aa2f33ed3f7f8eb764ca6
xo-shalaby-ox <mshalaby54@yahoo.com>
2025-09-26 11:51:00 +0300

    update some lines on home page and app.jsx

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <Link to="login">Login</Link>
-      <Link to="signup">Signup</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
 
       {/* <BackgroundBeams /> */}
     </>
