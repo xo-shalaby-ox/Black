da800790d41bda6e890bfd0ffc780eacc4f4b8ce
xo-shalaby-ox <mshalaby54@yahoo.com>
2025-09-29 20:01:03 +0300

    updates routes and home page

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <Link to="/" className="cursor-pointer">
+      <Link to="/" className="relative cursor-pointer z-40">
         home
       </Link>
 
